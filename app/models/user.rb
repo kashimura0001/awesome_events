fcafd5f708dfffc4f5f0fa7d39ad3978564331e5
keisukekashimura <kashimura@refcome.com>
2020-08-10 16:16:52 +0900

    added event details page

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
     provider = auth_hash[:provider]
     uid = auth_hash[:uid]
     nickname = auth_hash[:info][:nickname]
-    image_url = auth_hash[:info][:nickname]
+    image_url = auth_hash[:info][:image]
 
     User.find_or_create_by!(provider: provider, uid: uid) do |user|
       user.name = nickname
